09949b3ddb3612bd0fb01a48ded8f86ab364757e
Cobeaquen <cobeaquen@gmail.com>
2020-05-06 16:32:14 +0200

    Picture cycle

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
 var index = 0;
-var imgs = ['bilder\\bildspel\\img1.jpg', 'bilder\\bildspel\\img2.jpg', 'bilder\\bildspel\\img3.jpg']
-var imgElement;
+var index2 = 1;
+var imgs = ['bilder/bildspel/img0.jpg', 'bilder/bildspel/img1.jpg', 'bilder/bildspel/img2.jpg', 'bilder/bildspel/img3.jpg']
+var newImgElem = document.getElementById('nybild');
+var imgElem = document.getElementById('bild');
+newImgElem.addEventListener('animationiteration', cycleImage);
 
-function imageCycle() {
-    imgs.forEach().call(function(v, i) {
-        imgs[i].hidden = i !== index
-    });
-    index = (index + 1) % document.images.length;
+function cycleImage() {
+    console.log('Byter bild...');
+
+    newImgElem.backgroundImage = null;
+
+    imgElem.style.backgroundImage = newImgElem.style.backgroundImage;
+
+    //newImgElem.style.backgroundImage = "url("+ imgs[index2] + ")";
+    //imgElem.style.backgroundImage = "url("+ imgs[index] + ")";
+
+    newImgElem.style.animationPlayState = "paused";
+    imgElem.style.animationPlayState = "paused";
+
+    setTimeout(nextImg, 3000);
+}
+
+function nextImg() {
+    imgElem.style.backgroundImage = "url("+ imgs[index] + ")";
+    newImgElem.style.backgroundImage = "url("+ imgs[index2] + ")";
+
+    
+
+    newImgElem.style.animationPlayState = "running";
+    imgElem.style.animationPlayState = "running";
+
+    index = (index + 1) % imgs.length;
+    index2 = (index2 + 1) % imgs.length;
 }
 
 window.onload = function() {
-    setInterval(this.imageCycle(), 1000)
-    this.imgElement = document.getElementById('bildspel');
+    //setInterval(this.imageCycle, 3000)
+    this.nextImg();
 };
